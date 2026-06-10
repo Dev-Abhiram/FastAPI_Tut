17a9b480667efce2f34946ae72bf5795c5c85409
Abhiram <abhiramap05@gmail.com>
2026-06-10 12:23:59 +0530

    started fastapi

---
+++ fastapi_tut/1_Intro/intro.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+posts :list[dict]=[
+    
+        {
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+  },
+    
+]
+
+@app.get("/",response_class=HTMLResponse,include_in_schema=False)
+@app.get("/posts",include_in_schema=False)
+def home():
+      return f"<h1>{posts[0]['title']}</h1>"
+
+
+@app.get("/api/posts")
+def get_posts():
+  return posts
